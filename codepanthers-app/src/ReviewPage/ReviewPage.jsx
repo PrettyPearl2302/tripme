65354a2d83607075c4f1e4d588bed9ef463d6d3e
Pearl Wogu <wogup@Pearls-MacBook-Air.local>
2023-11-05 06:30:28 -0600

    add first part of review page

--- codepanthers-app/src/ReviewPage/ReviewPage.jsx
+++ codepanthers-app/src/ReviewPage/ReviewPage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-
 export default function ReviewPage () {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <><div>
+              <div className="help-tripme">Help Tripme</div>
+              <div className="reduce">Reduce</div>
+              <div className='food'>Food Wastage</div>
+              <div className='details'>Despite a growing in-flight catering market, valued at $18 billion in 2019 with expectations to reach $24 billion by 2027, 86% of airlines are planning sustainability investments by 2024, which may include efforts to minimize food waste.</div>
+              <div>
+                  <img className="placeholder-image" src="https://www.realsimple.com/thmb/RxAC0RVmFtJcqEekPwqs5GutS0A=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/easy-dinner-recipes-f768402675e04452b1531360736da8b5.jpg" alt="Placeholder Image" />
+              </div>
+
+          </div></>
   );
 };
