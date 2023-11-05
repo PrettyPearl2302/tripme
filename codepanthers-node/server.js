7b4a0709c3e4306005a23612fb0b2857ea53d3b0
Pearl Wogu <wogup@Pearls-MacBook-Air.local>
2023-11-04 22:56:23 -0500

    successfully seeded database

---
+++ codepanthers-node/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import { sequelize } from './database.js';
+import { User} from './models/index.js';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json()); // Middleware for parsing JSON bodies from HTTP requests
+app.use(morgan())
+
+
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.findAll();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+
+
+const port = process.env.PORT || 3000;
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App is listening on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error('Unable to connect to the database:', error);
+  });
